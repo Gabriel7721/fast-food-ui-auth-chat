2735c74f119045beff8ddd306b34ba1d607ea6ab
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-01-26 16:38:41 +0700

    update Chat types, bao gồm type chatMessage, HelloPayload, ChatNewPayload

---
+++ src/types/chat-types.ts
@@ -0,0 +1,17 @@
+export type ChatMessage = {
+  id: string;
+  text: string;
+  at: number;
+  from: string;
+};
+
+export type HelloPayload = {
+  type: "Websocket is connected successfully";
+  you: { name: string; userId?: string; email?: string };
+  chat_history: ChatMessage[];
+};
+
+export type ChatNewPayload = {
+  type: "chat.new";
+  msg: ChatMessage;
+};
