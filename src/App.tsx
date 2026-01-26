3e76a8461358ba9e56444740552adb1c84a1b745
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-01-26 15:57:22 +0700

    update app.tsx để thêm xác thực JWT

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Review from "./pages/Review";
 import Home from "./pages/Home";
+import { AuthProvider } from "./auth/AuthContext";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/review" element={<Review />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/review" element={<Review />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 export default App;
